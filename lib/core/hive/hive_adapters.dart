c0d5ab06444c5d8b9c2a7539264d1415844dabf0
andi <andimendik@gmail.com>
2026-03-24 13:42:38 +0700

    update hive_adapter

--- lib/core/hive/hive_adapters.dart
+++ lib/core/hive/hive_adapters.dart
@@ -1,4 +1,6 @@
 import 'package:artakula/features/accounts/data/models/account.dart';
+import 'package:artakula/features/categories/data/models/category.dart';
+import 'package:artakula/features/transactions/data/models/transaction.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import '../../../features/expenses/data/models/expense.dart';
 
@@ -7,7 +9,12 @@ Future<void> registerHiveAdapters() async {
 
   Hive.registerAdapter(AccountAdapter());
   Hive.registerAdapter(ExpenseAdapter());
+  Hive.registerAdapter(TransactionAdapter());
+  Hive.registerAdapter(TransactionTypeAdapter());
+  Hive.registerAdapter(CategoryAdapter());
 
   await Hive.openBox<Account>('accounts');
   await Hive.openBox<Expense>('expenses');
+  await Hive.openBox<Transaction>('transactions');
+  await Hive.openBox<Category>('categories');
 }
